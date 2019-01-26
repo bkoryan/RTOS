636476070f2f8838d81658b6ef0127a79a38bf3e
Burak <b.fk@unb.ca>
2019-01-25 21:23:36 -0400

    Update Appen1.cpp

--- Lab2/Appen1.cpp
+++ Lab2/Appen1.cpp
@@ -56,25 +56,58 @@ Serial pc(USBTX, USBRX);
 int main()
 {
   char x;
-// Start execution of the thread: WatchdogThread with ID, WatchdogId:
+/*
+    osThread : Access to the thread definition for the function osThreadCreate.
+    osThreadCreate :
+    more can be read at https://www.keil.com/pack/doc/cmsis/RTOS/html/group__CMSIS__RTOS__ThreadMgmt.html#gaf0c7c6b5e09f8be198312144b5c9e453
+    osThreadCreate: osThread(WatchdogThread): thread definition referenced with osThread.
+                    "NULL":pointer that is passed to the thread function as start argument.In this example,no pointer passed.
+*/
   WatchdogId = osThreadCreate(osThread(WatchdogThread), NULL);
-// Start the watch dog timer and enable the watch dog interrupt
+
+/*
+osTimerId : structure for given timer id as oneshot.
+osTimerCreate : "osTimer(Wdtimer)-> name of the timer object is Wdtimer"
+                osTimerOnce : means one shot timer.for one-shot or osTimerPeriodic for periodic behavior.
+                (void*)0 : argument to the timer call back function.
+More can be read at https://www.keil.com/pack/doc/cmsis/RTOS/html/group__CMSIS__RTOS__TimerMgmt.html#ga1b8d670eaf964b2910fa06885e650678
+*/
   osTimerId OneShot = osTimerCreate(osTimer(Wdtimer), osTimerOnce, (void *)0);
+
   pc.printf("\r\n Hello World - RTOS Template Program 1");
-    do {
+
+/*
+pc.readable() : Determine if there is a character available to read.
+pc.getc (): get character into char variable x
+pc.putc() : print character in char variable x
+if the character 'r' is entered,then the led1 is turned off and the timer restarted
+osTimerStart: OneShot : Timer ID obtained by osTimerCreate
+              "2000" : 2000 miliseconds time delay value
+Thread::wait : go to sleep for 500 miliseconds.It can be set max 32-bit value
+
+*/
+do {
         if (pc.readable())
         {
         x=pc.getc();
-        pc.putc(x); //Echo keyboard entry
-        if(x=='r') led1=0; // Turn LED off.
-          osTimerStart(OneShot, 2000); // Start or restart the watchdog timer interrupt and set to 2000ms.
+        pc.putc(x);
+        if(x=='r') led1=0;
+          osTimerStart(OneShot, 2000);
         }
-          Thread::wait(500); // Go to sleep for 500 ms
+          Thread::wait(500);
     }while(1);
 }
 
 void WatchdogThread(void const *argument)
 {
+/*
+    while(true) : infinite loop.Same as do{....}while(1);
+    osSignalWait:
+          SignalWatchdog :Integer value."wait until all specified signal flags set or 0 for any single signal flag"
+          osWaitForever  : the function will wait for an infinite time until a specified signal is set.
+    More can be read at https://www.keil.com/pack/doc/CMSIS/RTOS/html/group__CMSIS__RTOS__SignalMgmt.html#ga38860acda96df47da6923348d96fc4c9
+    'led1 = 1' : turn on led1 LED
+*/
     while (true) {
       osSignalWait(SignalWatchdog, osWaitForever); // Go to sleep until a signal, SignalWatchdog, is received
       led1 = 1;
@@ -82,6 +115,14 @@ void WatchdogThread(void const *argument)
 }
 
 //*********************************************************************************************************
+/*
+Watchdog Interrupt service routine:
+-> a constant variable pointer named n is passed in
+WatchdogId : thread ID obtained by osThreadCreate or osThreadGetId.
+"0x1": specifies the signal flags of the thread that should be set.It can be 32-bit integer
+More can be read at : https://www.keil.com/pack/doc/CMSIS/RTOS/html/group__CMSIS__RTOS__SignalMgmt.html#ga3de2730654589d6c3559c4b9e2825553
+*/
+
 void WatchdogISR(void const *n)
 {
  osSignalSet(WatchdogId,0x1); // Send signal to thread with ID, WatchdogId, i.e., WatchdogThread.
