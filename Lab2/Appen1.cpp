028516c9596cf39337c89a1dee46aafd73e79931
Burak <b.fk@unb.ca>
2019-01-25 20:23:16 -0400

    Update Appen1.cpp

--- Lab2/Appen1.cpp
+++ Lab2/Appen1.cpp
@@ -1,1 +1,7 @@
+######################################################################################################################
+# Burak Koryan | bkoryan@unb.ca | January 24 2019
+# Lab notes for ECE 4333 - Robotics | Faculty of Engineering at University of New Brunswick,Fredericton,Canada.
+# Laboratory Experiment 2 : Applications of Real Time Systems Concepts by Prof.Chris Diduch at UNB 
+######################################################################################################################
+
 #include "mbed.h"
