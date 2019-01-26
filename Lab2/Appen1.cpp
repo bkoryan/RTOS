8709778e69d9cd24a09df54bba6c45578cad2fa3
Burak <b.fk@unb.ca>
2019-01-25 20:28:37 -0400

    Update Appen1.cpp

--- Lab2/Appen1.cpp
+++ Lab2/Appen1.cpp
@@ -6,3 +6,53 @@
 
 #include "mbed.h"
 #include "rtos.h"
+
+// Function prototypes
+void WatchdogISR(void const *n);
+void WatchdogThread(void const *argument);
+
+// Processes and threads
+int32_t SignalWatchdog;
+osThreadId WatchdogId;
+osTimerDef(Wdtimer, WatchdogISR); // Declare a watch dog timer
+osThreadDef(WatchdogThread, osPriorityRealtime, 1024); // Declare WatchdogThread as a thread/process
+
+// IO Port Configuration
+DigitalOut led1(LED1);
+Serial pc(USBTX, USBRX); // Pins (tx, rx) for PC serial channel
+
+//*********************************************************************************************************
+int main() 
+{ 
+  char x;
+// Start execution of the thread: WatchdogThread with ID, WatchdogId:
+  WatchdogId = osThreadCreate(osThread(WatchdogThread), NULL);
+// Start the watch dog timer and enable the watch dog interrupt
+  osTimerId OneShot = osTimerCreate(osTimer(Wdtimer), osTimerOnce, (void *)0);
+  pc.printf("\r\n Hello World - RTOS Template Program 1");
+    do {
+        if (pc.readable())
+        {
+        x=pc.getc();
+        pc.putc(x); //Echo keyboard entry
+        if(x=='r') led1=0; // Turn LED off.
+          osTimerStart(OneShot, 2000); // Start or restart the watchdog timer interrupt and set to 2000ms.
+        }
+          Thread::wait(500); // Go to sleep for 500 ms
+    }while(1);
+}
+// ******** Watchdog Thread ********
+void WatchdogThread(void const *argument)
+{
+    while (true) {
+      osSignalWait(SignalWatchdog, osWaitForever); // Go to sleep until a signal, SignalWatchdog, is received
+      led1 = 1;
+    }
+}
+
+//*********************************************************************************************************
+// ******** Watchdog Interrupt Handler ********
+void WatchdogISR(void const *n) 
+{
+ osSignalSet(WatchdogId,0x1); // Send signal to thread with ID, WatchdogId, i.e., WatchdogThread.
+}
